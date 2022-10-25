55327a10c2c7831bb391408a4d0cdb851bd6e32f
HenriqueSavio <henriquesaviots@gmail.com>
2022-10-25 20:30:29 -0300

    bootstrap adicionado

--- src/components/Questao02.jsx
+++ src/components/Questao02.jsx
@@ -1,54 +1,57 @@
 import React from 'react'
 import { useState } from 'react';
+import { Button } from 'reactstrap';
 
 const Questao02 = () => {
 
-    const[numero1, setNumero1] = useState(0);
-    const[numero2, setNumero2] = useState(0);
-    const[resposta, setResposta] = useState();
-         
-    const soma = () =>{
+    const [numero1, setNumero1] = useState(0);
+    const [numero2, setNumero2] = useState(0);
+    const [resposta, setResposta] = useState();
+
+    const soma = () => {
         setResposta(numero1 + numero2);
     }
-    const subtracao = () =>{
+    const subtracao = () => {
         setResposta(numero1 - numero2);
     }
-    const divisao = () =>{
+    const divisao = () => {
         setResposta(numero1 / numero2);
     }
-    const multiplicacao = () =>{
+    const multiplicacao = () => {
         setResposta(numero1 * numero2);
     }
 
     return (
-    <div>
-        <h1>Questão 02</h1>             
-        <ul>
-            <div>
-                <input type="number" placeholder='Número 1' onChange={((e) => setNumero1(e.target.valueAsNumber))}></input>
+        <div>
+            <h1>Questão 02</h1>
 
-                <input type="number" placeholder='Número 2' onChange={  ((e) => setNumero2(e.target.valueAsNumber))}></input>
-            </div>
-            
-            <div>
-                <button onClick={soma}>+</button>
+            <ul>
+                <div>
+                    <input type="number" placeholder='Número 1' onChange={((e) => setNumero1(e.target.valueAsNumber))}></input>
 
-                <button onClick={subtracao}>-</button>
+                    <input type="number" placeholder='Número 2' onChange={((e) => setNumero2(e.target.valueAsNumber))}></input>
+                </div>
 
-                <button onClick={multiplicacao}>*</button>
+                <div>
+                    <button onClick={soma}>+</button>
 
-                <button onClick={divisao}>/</button>
-            </div>
+                    <button onClick={subtracao}>-</button>
 
-            <div>
-                <h4>
-                    Resultado: {resposta}
-                </h4>
-            </div>
-        </ul>
-    </div>
+                    <button onClick={multiplicacao}>*</button>
+
+                    <button onClick={divisao}>/</button>
+
+                </div>
+
+                <div>
+                    <h4>
+                        Resultado: {resposta}
+                    </h4>
+                </div>
+            </ul>
+        </div>
 
     )
-} 
+}
 
 export default Questao02;
